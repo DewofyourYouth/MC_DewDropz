fd61545590c1058a6fa4ad1a5a00c357271b074f
Jacob E. Shore <jacobshore@gmail.com>
2017-08-30 04:18:40 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,181 @@
 <?php include 'header.php'; ?>
-
+<div class="row">
+        <div class="col-12">
+            <h2 class="text-center display-4">M.C. DewDropz: Rap</h2>
+            &nbsp;
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+        <figure class="figure">
+        <img src="http://placeskull.com/400/300/000000/3" class="figure-img img-fluid" alt="MC DewDropz">
+        <figcaption class="figure-caption text-center">
+            <strong>Title</strong><br>
+            Artist<br>
+            Description<br>
+            Tags
+        </figcaption>
+    </figure>        </div>
+        <div class="col-md-6">
+        <figure class="figure">
+        <img src="http://placeskull.com/400/300/000000/1" class="figure-img img-fluid" alt="MC DewDropz">
+        <figcaption class="figure-caption text-center">
+            <strong>Title</strong><br>
+            Artist<br>
+            Description<br>
+            Tags
+        </figcaption>
+    </figure>        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <h2 class="text-center display-5">M.C. DewDropz: Rap</h2>
+            &nbsp;
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3">
+        <figure class="figure">
+        <img src="http://placeskull.com/200/150/000000/3" class="figure-img img-fluid" alt="MC DewDropz">
+        <figcaption class="figure-caption">
+            <strong>Title</strong><br>
+            Artist<br>
+            Description<br>
+            Tags
+        </figcaption>
+    </figure>        </div>
+        <div class="col-md-3">
+        <figure class="figure">
+        <img src="http://placeskull.com/200/150/000000/1" class="figure-img img-fluid" alt="MC DewDropz">
+        <figcaption class="figure-caption">
+            <strong>Title</strong><br>
+            Artist<br>
+            Description<br>
+            Tags
+        </figcaption>
+    </figure>        </div>
+        <div class="col-md-3">
+        <figure class="figure">
+                <img src="http://placeskull.com/200/150/000000/2" class="figure-img img-fluid" alt="MC DewDropz">
+                <figcaption class="figure-caption">
+                    <strong>Title</strong><br>
+                    Artist<br>
+                    Description<br>
+                    Tags
+                </figcaption>
+            </figure>
+        </div>
+        <div class="col-md-3">
+            <figure class="figure">
+                <img src="http://placeskull.com/200/150/000000/4" class="figure-img img-fluid" alt="MC DewDropz">
+                <figcaption class="figure-caption">
+                    <strong>Title</strong><br>
+                    Artist<br>
+                    Description<br>
+                    Tags
+                </figcaption>
+            </figure>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <h2 class="text-center display-5">The Dewdlers: Bluegrass</h2>
+            &nbsp;
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3">
+        <figure class="figure">
+        <img src="http://placeskull.com/200/150/000000/3" class="figure-img img-fluid" alt="MC DewDropz">
+        <figcaption class="figure-caption">
+            <strong>Title</strong><br>
+            Artist<br>
+            Description<br>
+            Tags
+        </figcaption>
+    </figure>        </div>
+        <div class="col-md-3">
+        <figure class="figure">
+        <img src="http://placeskull.com/200/150/000000/1" class="figure-img img-fluid" alt="MC DewDropz">
+        <figcaption class="figure-caption">
+            <strong>Title</strong><br>
+            Artist<br>
+            Description<br>
+            Tags
+        </figcaption>
+    </figure>        </div>
+        <div class="col-md-3">
+        <figure class="figure">
+                <img src="http://placeskull.com/200/150/000000/2" class="figure-img img-fluid" alt="MC DewDropz">
+                <figcaption class="figure-caption">
+                    <strong>Title</strong><br>
+                    Artist<br>
+                    Description<br>
+                    Tags
+                </figcaption>
+            </figure>
+        </div>
+        <div class="col-md-3">
+            <figure class="figure">
+                <img src="http://placeskull.com/200/150/000000/4" class="figure-img img-fluid" alt="MC DewDropz">
+                <figcaption class="figure-caption">
+                    <strong>Title</strong><br>
+                    Artist<br>
+                    Description<br>
+                    Tags
+                </figcaption>
+            </figure>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <h2 class="text-center display-5">DJ D&euml;w: Trance</h2>
+            &nbsp;
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3">
+        <figure class="figure">
+        <img src="http://placeskull.com/200/150/000000/3" class="figure-img img-fluid" alt="MC DewDropz">
+        <figcaption class="figure-caption">
+            <strong>Title</strong><br>
+            Artist<br>
+            Description<br>
+            Tags
+        </figcaption>
+    </figure>        </div>
+        <div class="col-md-3">
+        <figure class="figure">
+        <img src="http://placeskull.com/200/150/000000/1" class="figure-img img-fluid" alt="MC DewDropz">
+        <figcaption class="figure-caption">
+            <strong>Title</strong><br>
+            Artist<br>
+            Description<br>
+            Tags
+        </figcaption>
+    </figure>        </div>
+        <div class="col-md-3">
+        <figure class="figure">
+                <img src="http://placeskull.com/200/150/000000/2" class="figure-img img-fluid" alt="MC DewDropz">
+                <figcaption class="figure-caption">
+                    <strong>Title</strong><br>
+                    Artist<br>
+                    Description<br>
+                    Tags
+                </figcaption>
+            </figure>
+        </div>
+        <div class="col-md-3">
+            <figure class="figure">
+                <img src="http://placeskull.com/200/150/000000/4" class="figure-img img-fluid" alt="MC DewDropz">
+                <figcaption class="figure-caption">
+                    <strong>Title</strong><br>
+                    Artist<br>
+                    Description<br>
+                    Tags
+                </figcaption>
+            </figure>
+        </div>
+    </div>
 <?php include 'footer.php'; ?>
+
